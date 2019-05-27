adc2ab5f9eb6f33316916b234554017c64366ae4
gousi54 <42952588+gousi54@users.noreply.github.com>
2019-05-27 09:35:26 +0900

    add *

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	Renderer [] rn;
+	void Start () {
+
+		rn = gameObject.GetComponentsInChildren<Renderer> ();
+	}
+
+	public void Red(){
+
+		rn [0].material.color = Color.red;
+	}
+
+	public void Blue(){
+
+		rn [0].material.color = Color.blue;
+	}
+
+	public void White(){
+
+		rn [0].material.color = Color.white;
+	}
+
+	public void Black(){
+
+		rn [0].material.color = Color.black;
+
+	}
+}
+
